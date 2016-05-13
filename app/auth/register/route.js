b725a3c5582c80447c863300579c1b884bb88106
danielxu <xwd.forever@gmail.com>
2016-05-13 19:57:45 -0400

    add validations

--- app/auth/register/route.js
+++ app/auth/register/route.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
+// const { get } = Ember;
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+
+  model() {
+    return this.store.createRecord('user');
+  },
   actions: {
     doRegister() {
       console.log(123);
+      // let {email, password} = getProperties(this, model);
+      // get(this, 'session')
+        // .authenticate('authenticator:oath2', name, password)
+        // .then(() => {
+
+
+        // })
     }
   }
 });
